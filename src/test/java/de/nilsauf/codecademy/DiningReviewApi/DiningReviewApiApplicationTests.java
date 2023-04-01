20cf32bc288790619b9f33c13e65e7241f37294a
Nils Aufschläger <mail@nilsauf.de>
2023-04-01 15:35:49 +0200

    add init files from spring initializr

---
+++ src/test/java/de/nilsauf/codecademy/DiningReviewApi/DiningReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.nilsauf.codecademy.DiningReviewApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
