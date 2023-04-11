7641308dfbd0d6139d72b9094ca8d4682d2cbfb0
Nils Aufschläger <mail@nilsauf.de>
2023-04-11 20:47:17 +0200

    add dining review repository

---
+++ src/main/java/de/nilsauf/codecademy/DiningReviewApi/Repositories/DiningReviewRepository.java
@@ -0,0 +1,11 @@
+package de.nilsauf.codecademy.DiningReviewApi.Repositories;
+
+import de.nilsauf.codecademy.DiningReviewApi.Entities.DiningReview;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface DiningReviewRepository extends CrudRepository<DiningReview, Long> {
+    List<DiningReview> findAllByRestaurantId(Long restaurantId);
+    List<DiningReview> finAllByState(DiningReview.DiningReviewState state);
+}
